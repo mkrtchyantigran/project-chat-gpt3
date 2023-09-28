6de76fa70267ed1e5136fdfa6abbd22590b6617c
Tigran <110290863+mkrtchyantigran@users.noreply.github.com>
2023-09-28 21:09:35 +0200

    project commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import{Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, NavBar, Feature } from './components';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <NavBar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    )
+}
+
+
+
+export default App;
